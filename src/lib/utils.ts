e628b05e3e0a33c29db943a1ada146d7d4754684
Dipl.-Ing. (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2026-06-27 21:41:02 -0400

    Add project config files (vite, tsconfig, components, supabase, utils)

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
